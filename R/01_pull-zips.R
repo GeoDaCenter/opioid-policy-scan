a029cdf180f407d66e44adb57d0de8bd47ffde17
Angela Li <=>
2019-10-21 15:19:26 -0500

    Fix output location

--- R/01_pull-zips.R
+++ R/01_pull-zips.R
@@ -7,4 +7,4 @@ zips_sf <- st_as_sf(zips, coords = c("INTPTLAT10", "INTPTLON10")) %>%
   st_transform(32616) # takes like 20 seconds
 
 st_write(zips_sf, "data-output/zips.gpkg")
-saveRDS(zips_sf, "zips.rds")
+saveRDS(zips_sf, "data-output/zips.rds")
