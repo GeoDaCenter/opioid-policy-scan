7f9f847a41f9cfd922f101db49e77c81346a75cd
Angela Li <=>
2019-10-21 09:50:57 -0500

    Include sf call

--- R/01_pull-zips.R
+++ R/01_pull-zips.R
@@ -1,5 +1,7 @@
 # Get ZCTAs for Illinois
 
+library(sf)
+
 zips <- tigris::zctas(state = "Illinois") # takes like 5 min
 zips_sf <- st_as_sf(zips, coords = c("INTPTLAT10", "INTPTLON10")) %>%
   st_transform(32616) # takes like 20 seconds
