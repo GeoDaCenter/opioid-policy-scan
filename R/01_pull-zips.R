d4f71cb42dc24342d302613068073e118f5da814
Angela Li <=>
2019-10-21 15:06:03 -0500

    Fix zip code

--- R/01_pull-zips.R
+++ R/01_pull-zips.R
@@ -1,9 +1,10 @@
-# Get ZCTAs for Illinois
+# Get ZCTAs for Illinois and save object
 
 library(sf)
 
-zips <- tigris::zctas(state = "Illinois") # takes like 5 min
+zips <- tigris::zctas(starts_with = c("60", "61", "62")) # takes like 5 min, 1383 ZCTAs in IL
 zips_sf <- st_as_sf(zips, coords = c("INTPTLAT10", "INTPTLON10")) %>%
   st_transform(32616) # takes like 20 seconds
 
 st_write(zips_sf, "data-output/zips.gpkg")
+saveRDS(zips_sf, "zips.rds")
