59bc18977ca7583b5bb98c0e2d7b8da24581be50
Angela Li <=>
2019-10-28 14:24:15 -0500

    Clip counties to state shape

--- R/01_pull-geometries.R
+++ R/01_pull-geometries.R
@@ -3,6 +3,12 @@
 library(sf)
 library(tigris) # all geometries from year 2017
 
+# Illinois boundary
+illinois <- tigris::states(cb = TRUE) %>%
+  st_as_sf() %>% 
+  filter(NAME == "Illinois") %>%
+  st_transform(32616)
+
 # 1383 ZCTAs in Illinois
 zips <- zctas(starts_with = c("60", "61", "62")) # takes like 5 min
 zips_sf <- st_as_sf(zips, coords = c("INTPTLON10", "INTPTLAT10")) %>%
@@ -12,6 +18,9 @@ zips_sf <- st_as_sf(zips, coords = c("INTPTLON10", "INTPTLAT10")) %>%
 il_counties <- counties(state = "Illinois")
 il_counties_sf <- st_as_sf(il_counties, coords = c("INTPTLON", "INTPTLAT")) %>% 
   st_transform(32616)
+# Clip to IL shape
+il_counties_sf <- st_intersection(il_counties_sf, illinois)
+# plot(il_counties_sf["geometry"])
 
 # 13 MSAs in Illinois
 il_msas <- core_based_statistical_areas(class = "sf") %>% 
@@ -22,11 +31,6 @@ il_msas <- core_based_statistical_areas(class = "sf") %>%
   select(name) %>%
   add_column(urban = TRUE)
 
-illinois <- tigris::states(cb = TRUE) %>%
-  st_as_sf() %>% 
-  filter(NAME == "Illinois") %>%
-  st_transform(32616)
-
 # 45 Counties Bordering Illinois 
 il_border_counties <- counties(state = c("Indiana", "Wisconsin", "Iowa", "Missouri", "Kentucky"))
 il_border_counties_sf <- st_as_sf(il_border_counties, coords = c("INTPTLON", "INTPTLAT")) %>% 
@@ -34,6 +38,19 @@ il_border_counties_sf <- st_as_sf(il_border_counties, coords = c("INTPTLON", "IN
 
 il_border_counties_sf <- il_border_counties_sf[lengths(st_touches(il_border_counties_sf, il_counties_sf)) > 0,]
 # plot(il_border_counties_sf["geometry"])
+
+# Clip to Wisconsin boundary
+wisconsin <- tigris::states(cb = TRUE) %>%
+  st_as_sf() %>% 
+  filter(NAME == "Wisconsin") %>%
+  st_transform(32616)
+
+in_wisc <- filter(il_border_counties_sf, STATEFP == "55")
+not_in_wisc <- filter(il_border_counties_sf, STATEFP != "55")
+wisc_border_counties <- st_intersection(in_wisc, wisconsin["geometry"])
+
+border_counties <- rbind(wisc_border_counties, not_in_wisc)
+# plot(border_counties["geometry"])
 
 # Save final versions ------------------------------------------------------
 
@@ -47,4 +64,4 @@ st_write(il_msas, "data-output/il-msas.gpkg", delete_dsn = TRUE)
 
 st_write(illinois, "data-output/illinois.gpkg", delete_dsn = TRUE)
 
-st_write(il_border_counties_sf, "data-output/il_border_counties.gpkg", delete_dsn = TRUE)
+st_write(border_counties, "data-output/il_border_counties.gpkg", delete_dsn = TRUE)
