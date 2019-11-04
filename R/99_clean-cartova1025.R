fb15f657fbccdbf991bda713d14b7b2ec3aa8500
Angela Li <=>
2019-11-04 16:04:44 -0600

    Validate zip is same as zcta in master zip data

--- R/99_clean-cartova1025.R
+++ R/99_clean-cartova1025.R
@@ -6,6 +6,9 @@ zips_sf <- readRDS("data-output/zips.rds") %>%
   mutate(zip = as.numeric(ZCTA5CE10)) %>% 
   select(zip)
 
+# # Double check that zip is same as zcta, so can join on it
+# identical(cartova_1025$zip, cartova_1025$zcta)
+
 cartova_1025_sf <- zips_sf %>% 
   right_join(cartova_1025, by = "zip")
 
