2617c902e4b9b57a8b66c97269059a360a32c1c4
Angela Li <=>
2020-01-15 16:39:53 -0600

    Add prelim prisons webscraping script

---
+++ R/_01-get-il-prisons.R
@@ -0,0 +1,30 @@
+library(rvest)
+library(dplyr)
+library(purrr)
+library(readr)
+
+prison_url <- read_html("https://www2.illinois.gov/idoc/facilities/Pages/correctionalfacilities.aspx")
+
+prison_names <- html_nodes(prison_url, "a") %>% 
+  html_text() %>% 
+  .[93:120]
+
+prison_links <- html_nodes(prison_url, "a") %>% 
+  html_attr("href") %>% 
+  .[93:120]
+
+
+get_address <- function(url) {
+  read_html(url) %>% 
+    html_nodes("#ctl00_PlaceHolderMain_ctl07__ControlWrapper_RichHtmlField p:nth-child(1)") %>% 
+    html_text()
+}
+
+prison_df <- cbind(prison_names, prison_links) %>% 
+  as.data.frame(stringsAsFactors = FALSE) 
+
+prison_address_df <- prison_df %>% 
+  mutate(address = map(prison_links, get_address)) %>% 
+  mutate(address = as.character(address))
+
+write_csv(prison_address_df, "data-output/il_prisons.csv")
