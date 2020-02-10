695979aec4f2eddcb0825ca6320ab56863103c47
Angela Li <angelali921@gmail.com>
2020-02-10 16:57:25 -0600

    Update maps of access metrics

--- R/05_map-access-metrics.R
+++ R/05_map-access-metrics.R
@@ -5,11 +5,12 @@ library(dplyr)
 library(sf)
 library(tmap)
 
-access <- read_csv("data/access_by_type_subset_buffer.csv") %>% 
-  rename(zip = `- geoid`) %>% 
+access <- read_csv("data/access_by_zip_moud_subset.csv") %>% 
+  rename(zip = `origin`) %>% 
   mutate(zip = as.character(zip))
 zips <- readRDS("data-output/zips.rds")
 moud <- read_sf("data-output/02_moud-all.gpkg")
+bup <- read_sf("data-output/01_bup.gpkg")
 
 access_sf <- right_join(zips, access, by = c("GEOID10" = "zip"))
 
@@ -18,15 +19,23 @@ access_sf <- right_join(zips, access, by = c("GEOID10" = "zip"))
 # Make maps
 illinois <- read_sf("data-output/illinois.gpkg")
 
+tmap_mode("view")
+
 tm_shape(illinois) +
   tm_borders() +
   tm_shape(access_sf) +
-  tm_polygons(c("MOUD - Buprenorphine - count", "MOUD - Methadone - count", "MOUD - Naltrexone - count"))
+  tm_polygons(c("MOUD - Buprenorphine - count", 
+                "MOUD - Buprenorphine - avg_drive_minutes", 
+                "MOUD - Buprenorphine - avg_distance_km"), style = "quantile") +
+  tm_shape(bup) +
+  tm_dots(size = 0.1)
 
 tm_shape(illinois) +
   tm_borders() +
 tm_shape(access_sf) +
-  tm_polygons(c("MOUD - Buprenorphine - km", "MOUD - Methadone - km", "MOUD - Naltrexone - km"))
+  tm_polygons(c("MOUD - Buprenorphine - avg_distance_km", "MOUD - Methadone - avg_distance_km", "MOUD - Naltrexone - avg_distance_km"), style = "quantile") +
+  tm_shape(moud) +
+  tm_dots(size = 0.1)
 
 tm_shape(illinois) +
   tm_borders() +
