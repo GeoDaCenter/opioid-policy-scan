d44d54babfcc88d89927c4cf679845278cc05d62
Angela Li <angelali921@gmail.com>
2020-02-10 17:07:09 -0600

    Save maps

--- R/05_map-access-metrics.R
+++ R/05_map-access-metrics.R
@@ -11,38 +11,63 @@ access <- read_csv("data/access_by_zip_moud_subset.csv") %>%
 zips <- readRDS("data-output/zips.rds")
 moud <- read_sf("data-output/02_moud-all.gpkg")
 bup <- read_sf("data-output/01_bup.gpkg")
+meth <- read_sf("data-output/01_meth.gpkg")
+naltrex <- read_sf("data-output/01_naltrex.gpkg")
 
-access_sf <- right_join(zips, access, by = c("GEOID10" = "zip"))
+access_sf <- right_join(zips, access, by = c("GEOID10" = "zip")) %>% 
+  rename(bup_count = `MOUD - Buprenorphine - count`,
+         bup_avg_drive_min = `MOUD - Buprenorphine - avg_drive_minutes`,
+         bup_avg_dist_km = `MOUD - Buprenorphine - avg_distance_km`,
+         meth_count = `MOUD - Methadone - count`,
+         meth_avg_drive_min = `MOUD - Methadone - avg_drive_minutes`,
+         meth_avg_dist_km = `MOUD - Methadone - avg_distance_km`,
+         nal_count = `MOUD - Naltrexone - count`,
+         nal_avg_drive_min = `MOUD - Naltrexone - avg_drive_minutes`,
+         nal_avg_dist_km = `MOUD - Naltrexone - avg_distance_km`)
 
 
 
 # Make maps
 illinois <- read_sf("data-output/illinois.gpkg")
 
-tmap_mode("view")
+tmap_mode("plot")
 
-tm_shape(illinois) +
+bup_access <- 
+  tm_shape(illinois) +
   tm_borders() +
   tm_shape(access_sf) +
-  tm_polygons(c("MOUD - Buprenorphine - count", 
-                "MOUD - Buprenorphine - avg_drive_minutes", 
-                "MOUD - Buprenorphine - avg_distance_km"), style = "quantile") +
+  tm_polygons(c("bup_count", 
+                "bup_avg_drive_min", 
+                "bup_avg_dist_km"), style = "quantile") +
   tm_shape(bup) +
-  tm_dots(size = 0.1)
+  tm_dots(size = 0.1) +
+  tm_layout(main.title = "Buprenorphine Access Metrics")
 
-tm_shape(illinois) +
-  tm_borders() +
-tm_shape(access_sf) +
-  tm_polygons(c("MOUD - Buprenorphine - avg_distance_km", "MOUD - Methadone - avg_distance_km", "MOUD - Naltrexone - avg_distance_km"), style = "quantile") +
-  tm_shape(moud) +
-  tm_dots(size = 0.1)
-
-tm_shape(illinois) +
+meth_access <- 
+  tm_shape(illinois) +
   tm_borders() +
   tm_shape(access_sf) +
-  tm_polygons(c("MOUD - Buprenorphine - minutes", "MOUD - Methadone - minutes", "MOUD - Naltrexone - minutes")) +
-  tm_shape(moud) +
-  tm_dots(size = 0.1)
+  tm_polygons(c("meth_count", 
+                "meth_avg_drive_min", 
+                "meth_avg_dist_km"), style = "quantile") +
+  tm_shape(meth) +
+  tm_dots(size = 0.1) +
+  tm_layout(main.title = "Methadone Access Metrics")
+
+naltrex_access <-
+  tm_shape(illinois) +
+  tm_borders() +
+  tm_shape(access_sf) +
+  tm_polygons(c("nal_count", 
+                "nal_avg_drive_min", 
+                "nal_avg_dist_km"), style = "quantile") +
+  tm_shape(naltrex) +
+  tm_dots(size = 0.1) +
+  tm_layout(main.title = "Naltrexone Access Metrics")
+
+tmap_save(bup_access, "output/bup_access.png")
+tmap_save(meth_access, "output/meth_access.png")
+tmap_save(naltrex_access, "output/naltrex_access.png")
 
 # What's with the NAs??
 summary(access_sf)
