b44d3f5e076c37c83b508008f91983e79ef81cba
Angela Li <angelali921@gmail.com>
2020-06-16 14:32:58 -0500

    Calculate 18-64 population by zip code

---
+++ R/JCOIN/10_pop-by-zip.R
@@ -0,0 +1,20 @@
+# Get pop by zip code
+
+library(tidycensus)
+library(tidyverse)
+
+v18 <- load_variables(2018, "acs5", cache = TRUE)
+# View(v18)
+pop18_64vars <- c(v18$name[9:21], v18$name[33:45])
+
+pop18_64 <- get_acs(geography = "zcta",
+                    variables = pop18_64vars)
+
+head(pop18_64)
+
+# 2014-2018 ACS, ages 18-64 (could also do 15-64?)
+pop_by_zip <- pop18_64 %>% 
+  group_by(GEOID) %>% 
+  summarize(popest = sum(estimate))
+
+write_csv(pop_by_zip, "data-output/pop_by_zip18_64_2014_2018.csv")
