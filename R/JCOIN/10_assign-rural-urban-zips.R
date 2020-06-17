40a9c0172223250c3642c6efbab2456ef7ea8ef0
Angela Li <angelali921@gmail.com>
2020-06-16 19:03:51 -0500

    Add plots, update urban/rural assignment

--- R/JCOIN/10_assign-rural-urban-zips.R
+++ R/JCOIN/10_assign-rural-urban-zips.R
@@ -1,17 +1,12 @@
-# Assign zips to metro/micro areas
+# Summary stats by urban / rural
+# Non-spatial analysis
 
-library(sf)
-library(tidycensus)
 library(tidyverse)
 
-library(readxl)
-
-
-# Summary stats by urban / rural
-
+all_access_us <- read_csv("data-output/min_dists_all.csv")
 str(all_access_us)
 
-
+## Assign rurality by population density
 urban_rural <- mutate(all_access_us, 
                       ALAND10 = 
                         units::set_units(
@@ -22,7 +17,7 @@ urban_rural <- mutate(all_access_us,
                       popdensity = as.numeric(poptotal / mi2),
                       rurality = case_when(
                         popdensity < 1000 ~ "rural",
-                        popdensity >= 1000 & popdensity <= 3000 ~ "semi-urban",
+                        popdensity >= 1000 & popdensity <= 3000 ~ "suburban",
                         popdensity > 3000 ~ "urban"))
 
 r <- tm_shape(urban_rural) +
@@ -32,8 +27,77 @@ tmap_save(r, "output/rurality.png")
 beepr::beep()
 
 
+urban_rural %>% 
+  group_by(rurality) %>%
+  summarize(bup_dist = mean(bup),
+            bup_sd = sd(bup),
+            meth_dist = mean(meth),
+            meth_sd = sd(meth),
+            nal_dist = mean(nal),
+            nal_sd = sd(nal),
+            dialysis_dist = mean(dialysis),
+            dialysis_sd = sd(dialysis)) %>% 
+  mutate_if(is.numeric, round, 2) %>% 
+  gt::gt()
 
-## Unsuccessful attempts at using micro/metropolitan areas 
+
+# Pivot the summary stats
+urban_rural_long <- urban_rural %>% 
+  select(zip = ZCTA5CE10, dialysis, meth, bup, nal, rurality) %>% 
+  pivot_longer(-c(zip, rurality), names_to = "resource", values_to = "min_dist") %>% 
+  pivot_wider(names_from = rurality, values_from = min_dist) 
+  
+summary_stats <- urban_rural_long %>% 
+  group_by(resource) %>%
+  summarize(rural_avg_dist = median(rural, na.rm = TRUE),
+            rural_sd = sd(rural, na.rm = TRUE),
+            suburban_avg_dist = median(suburban, na.rm = TRUE),
+            suburban_sd = sd(suburban, na.rm = TRUE),
+            urban_avg_dist = median(urban, na.rm = TRUE),
+            urban_sd = sd(urban, na.rm = TRUE)
+            ) %>% 
+  mutate_if(is.numeric, round, 2)
+
+urban_rural %>% 
+  group_by(rurality) %>% 
+  count()
+
+gt::gt(summary_stats)
+
+write_csv(summary_stats, "data-output/urban_rural_min_dist_median.csv")
+
+p <- summary_stats %>% 
+  pivot_longer(-resource) %>% 
+  filter(!str_detect(name, "sd")) %>% 
+  ggplot(aes(x = resource, y = value)) +
+  geom_col() +
+  coord_flip() +
+  theme_minimal() +
+  facet_wrap(~name, ncol = 1) +
+  labs(title = "Median Distance to Nearest Resource (mi)",
+       y = "Distance to Nearest Resource (mi)")
+
+ggsave("output/median-dist-compare.png", width = 5, height = 4)
+
+ggplot(summary_stats, aes(x = resource, y = rural_avg_dist)) +
+  geom_col() +
+  geom_errorbar(aes(ymax = rural_avg_dist + rural_sd, ymin = rural_avg_dist - rural_sd)) +
+  coord_flip() +
+  theme_minimal()
+
+ggplot(summary_stats, aes(x = resource, y = suburban_avg_dist)) +
+  geom_col() +
+  geom_errorbar(aes(ymax = suburban_avg_dist + suburban_sd, ymin = suburban_avg_dist - suburban_sd)) +
+  coord_flip() +
+  theme_minimal()
+
+ggplot(summary_stats, aes(x = resource, y = urban_avg_dist)) +
+  geom_col() +
+  geom_errorbar(aes(ymax = urban_avg_dist + urban_sd, ymin = urban_avg_dist - urban_sd)) +
+  coord_flip() +
+  theme_minimal()
+
+## Unsuccessful attempts at using micro/metropolitan areas to assign 
 
 # metros <- tigris::metro_divisions() %>% 
 #   st_as_sf()
