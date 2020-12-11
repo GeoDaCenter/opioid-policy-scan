d9e2b205d81735edd66f1f3f91f5b39c6fa37c52
Olina <oliang@uchicago.edu>
2020-12-11 21:51:00 +0800

    Update rename_for_geocode.py

--- MOUD_scraping/rename_for_geocode.py
+++ MOUD_scraping/rename_for_geocode.py
@@ -3,8 +3,9 @@ import sys
 
 def main():
     df = pd.read_csv(sys.argv[1])
+    df = df.iloc[:, [0,3,6,7,8]]
     df = df.rename(columns={"Name1": "ID", "Address1": "ADDRESS", "City": "CITY", "State": "STATE", "ZIP_Code":"ZIP"})
-    df.to_csv("geo" + sys.argv[1], index = False)
+    df.to_csv('/'.join(sys.argv[1].split('/')[:-1]) + "/geo" + sys.argv[1].split('/')[-1], index = False)
 
 if __name__ == '__main__':
     sys.exit(main())
