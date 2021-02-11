6c44ca97525422013dc736b5150fd676d467c8a3
Qinyun Lin <qinyunlin@uchicago.edu>
2021-02-11 15:43:14 -0600

    Update 02_summarystat.R

--- MOUD_access/r/02_summarystat.R
+++ MOUD_access/r/02_summarystat.R
@@ -1,3 +1,28 @@
+describeBy(data, data$rurality)
+
+library(readr) 
+library(tidyverse)
+library(Hmisc)
+
+allaccess_SVI_rurality <- read_csv("data_final/allaccess_SVI_rurality.csv", col_types = cols(minDialysis = col_number()))
+
+data <- allaccess_SVI_rurality %>% 
+  select(starts_with("min"), starts_with("count"), starts_with("time"), ends_with("_score"), rurality)
+
+library(gtsummary)
+table <- tbl_summary(
+  data,
+  by = rurality, 
+  missing = "no",
+  digits = all_continuous() ~ 2,
+) %>% 
+  add_n() %>% 
+  add_p() %>% 
+  modify_header(label = "**Variable**") %>% # update the column header
+  bold_labels() 
+
+
+
 library(gtools)
 
 data$theme1_qr <- quantcut(data$RPL_THEME1, q = 4, na.rm = T)          
