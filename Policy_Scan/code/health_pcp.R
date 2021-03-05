a9663bb4b49f9004865692b83ace7bfb85cbb303
spaykin <spaykin@uchicago.edu>
2021-03-05 15:30:29 -0600

    Update PCP script and add raw data

--- Policy_Scan/code/health_pcp.R
+++ Policy_Scan/code/health_pcp.R
@@ -1,7 +1,15 @@
+##### ABOUT ----
+
+# This code loads and cleans data on primary care and specialist providers at the U.S. census tract, county, and state levels. 
+
+#### SET UP ----
+
 library(tidyverse)
 library(dplyr)
 library(tmap)
 library(sf)
+
+#### LOAD DATA ----
 
 # Read in data
 pcp_raw <- read.csv("Policy_Scan/data_raw/raw_pcp_t_103113_1.csv")
@@ -9,24 +17,12 @@ pcp_raw <- read.csv("Policy_Scan/data_raw/raw_pcp_t_103113_1.csv")
 names(pcp_raw)
 head(pcp_raw)
 
-# Clean data
+# Clean up data
 pcp_raw$STATE <- sprintf("%02d", pcp_raw$STATE)
 pcp_raw$COUNTY <- sprintf("%05d", pcp_raw$COUNTY)
 pcp_raw$TRACT <- sprintf("%06d", pcp_raw$TRACT)
 
-# Tract
-pcp_tract <- pcp_raw %>%
-  mutate(geoid = paste0(pcp$state, pcp$county, pcp$tract)) %>%
-  select(geoid, state = STATE, county = COUNTY, tract = TRACT, pcp_total = TG_DOC, sp_total = TS_DOC)
-
-# County
-pcp_county <- pcp_raw %>%
-  select(state = STATE, county = COUNTY, pcp_total = TG_DOC, sp_total = TS_DOC) %>%
-  group_by(state, county) %>%
-  summarise(pcp_total = sum(pcp_total),
-            sp_total = sum(sp_total))
-
-str(pcp_county)
+#### PREPARE FOR SPATIAL ANALYSIS ----
 
 # State
 pcp_state <- pcp_raw %>%
@@ -35,24 +31,22 @@ pcp_state <- pcp_raw %>%
   summarise(pcp_total = sum(pcp_total),
             sp_total = sum(sp_total))
 
-str(pcp_state)
+# County
+pcp_county <- pcp_raw %>%
+  select(state = STATE, county = COUNTY, pcp_total = TG_DOC, sp_total = TS_DOC) %>%
+  group_by(state, county) %>%
+  summarise(pcp_total = sum(pcp_total),
+            sp_total = sum(sp_total))
 
-# Save data
+# Tract
+pcp_tract <- pcp_raw %>%
+  mutate(geoid = paste0(pcp$state, pcp$county, pcp$tract)) %>%
+  select(geoid, state = STATE, county = COUNTY, tract = TRACT, pcp_total = TG_DOC, sp_total = TS_DOC)
+
+#### SAVE DATA ----
+
 write.csv(pcp_tract, "Policy_Scan/data_final/Health03_T.csv")
 write.csv(pcp_county, "Policy_Scan/data_final/Health03_C.csv")
 write.csv(pcp_state, "Policy_Scan/data_final/Health03_S.csv")
 
-
-# variable: TG_DOC 
-# Primary Care physicians
-# Number of clinically active Primary Care physicians in the area
-# Source: AMA, 2010
-# pg 5 - https://data.nber.org/dartmouthatlas/pcsa/Data_Dictionary_PCSAv3.1_Sept2013.pdf
-
-# variable: TS_DOC
-# Specialist physicians
-# Number of clinically active Specialist physicians in the area
-# Source: AMA, 2010
-# pg 5 - https://data.nber.org/dartmouthatlas/pcsa/Data_Dictionary_PCSAv3.1_Sept2013.pdf
-
-# Final variables:  geoid, stategp, countyfp, tractce, pcp_total, sp_total
+#### FIN ----
