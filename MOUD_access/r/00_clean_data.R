c8b876ae046185f68cd6007cac833f0bdebd189a
Qinyun Lin <qinyunlin@uchicago.edu>
2021-03-14 18:31:15 -0500

    update SVI with weighted mean based on total address

--- MOUD_access/r/00_clean_data.R
+++ MOUD_access/r/00_clean_data.R
@@ -17,9 +17,10 @@ data <- left_join(minDis, accesstoNalVivitrol, by = c("originGEOID" = "ZCTA"))
 
 meth_data_us <- read_csv("data_final/meth_access.csv", col_types = cols(ZCTA = col_character()))
 meth_data_us$X1 <- NULL
+meth_data_us$GEOID <- as.numeric(meth_data_us$ZCTA)
 
 ## data <- left_join(data, bup_data_us, by = c("ZIP_CODE" = "ZCTA"))
-data <- left_join(data, meth_data_us, by = c("ZIP_CODE" = "ZCTA"))
+data <- left_join(data, meth_data_us, by = c("originGEOID" = "GEOID"))
 
 ###summary(data$time_to_nearest_buprenorphine)
 ## data$time_to_nearest_buprenorphine <- 
@@ -32,28 +33,6 @@ data$time_to_nearest_methadone <-
 summary(data$`time_to_nearest_naltrexone/vivitrol`)
 data$`time_to_nearest_naltrexone/vivitrol` <- 
   ifelse(data$`time_to_nearest_naltrexone/vivitrol` == 999, NA, data$`time_to_nearest_naltrexone/vivitrol`)
-
-SVI2018_US <- read_csv("data_raw/SVI2018_US.csv") 
-SVI <- SVI2018_US %>% 
-  select(FIPS, RPL_THEME1, RPL_THEME2, RPL_THEME3, RPL_THEME4, RPL_THEMES)
-
-library(readxl)
-ZIP_TRACT_122020 <- read_excel("data_raw/ZIP_TRACT_122020.xlsx", 
-                               col_types = c("text", "text", "numeric", "numeric", "numeric", "numeric"))
-
-ZIP_TRACT <- ZIP_TRACT_122020 %>% 
-  group_by(ZIP) %>% 
-  filter(RES_RATIO == max(RES_RATIO)) %>% 
-  select(ZIP, TRACT)
-
-data <- left_join(data, ZIP_TRACT, by = c("ZIP_CODE" = "ZIP"))
-data <- left_join(data, SVI, by = c("TRACT" = "FIPS"))
-
-data$RPL_THEME1[data$RPL_THEME1 == -999] <- NA
-data$RPL_THEME2[data$RPL_THEME2 == -999] <- NA
-data$RPL_THEME3[data$RPL_THEME3 == -999] <- NA
-data$RPL_THEME4[data$RPL_THEME4 == -999] <- NA
-data$RPL_THEMES[data$RPL_THEMES == -999] <- NA
 
 minDistDialy <- read_csv("intmed_output/Access_Dialysis_Z.csv")
 
@@ -80,6 +59,47 @@ data <- left_join(data, access_Dialysis, by = c("originGEOID" = "ZCTA"))
 summary(data$time_to_nearest_dialysis)
 data$time_to_nearest_dialysis <- 
   ifelse(data$time_to_nearest_dialysis == 999, NA, data$time_to_nearest_dialysis)
+
+write_csv(data, "data_final/allaccess_SVI_rurality.csv")
+
+###### fix the problem for one zip code may relate to multiple tracts because of same RES_RATIO
+
+data <- read_csv("data_final/allaccess_SVI_rurality.csv")
+ 
+data <- data %>% 
+  select(-starts_with("RPL_THEME"), -TRACT) %>% 
+  distinct()
+
+library(readxl)
+ZIP_TRACT_122020 <- read_excel("data_raw/ZIP_TRACT_122020.xlsx", 
+                               col_types = c("text", "text", "numeric", "numeric", "numeric", "numeric"))
+
+ZIP_TRACT <- ZIP_TRACT_122020 %>%
+  select(ZIP, TRACT, TOT_RATIO)
+  
+SVI2018_US <- read_csv("data_raw/SVI2018_US.csv") 
+SVI <- SVI2018_US %>% 
+  select(FIPS, RPL_THEME1, RPL_THEME2, RPL_THEME3, RPL_THEME4, RPL_THEMES)
+
+SVI$RPL_THEME1[SVI$RPL_THEME1 == -999] <- NA
+SVI$RPL_THEME2[SVI$RPL_THEME2 == -999] <- NA
+SVI$RPL_THEME3[SVI$RPL_THEME3 == -999] <- NA
+SVI$RPL_THEME4[SVI$RPL_THEME4 == -999] <- NA
+SVI$RPL_THEMES[SVI$RPL_THEMES == -999] <- NA
+
+SVI_ZIP <- SVI %>% 
+  group_by(ZIP) %>% 
+  mutate(SVI1 = weighted.mean(RPL_THEME1, w = TOT_RATIO, na.rm = T),
+         SVI2 = weighted.mean(RPL_THEME2, w = TOT_RATIO, na.rm = T),
+         SVI3 = weighted.mean(RPL_THEME3, w = TOT_RATIO, na.rm = T),
+         SVI4 = weighted.mean(RPL_THEME4, w = TOT_RATIO, na.rm = T),
+         SVIS = weighted.mean(RPL_THEMES, w = TOT_RATIO, na.rm = T)) %>% 
+  select(ZIP, starts_with("SVI")) %>% 
+  distinct()
+ 
+SVI_ZIP$GEOID <- as.numeric(SVI_ZIP$ZIP) 
+data <- left_join(data, SVI_ZIP, by = c("originGEOID" = "GEOID")) %>% 
+  select(-ZIP_CODE, -ZCTA, -ZIP)
 
 write_csv(data, "data_final/allaccess_SVI_rurality.csv")
 
