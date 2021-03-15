582881f3a908298ccb6a8e9c4afedd58172a5552
spaykin <spaykin@uchicago.edu>
2021-03-15 16:46:55 -0500

    Re-calculated min. distance access for updated buprenorphine data
    Added updated raw moud dataset, updated metadata

---
+++ Policy_Scan/code/access_MOUDs.R
@@ -0,0 +1,152 @@
+#### About ----
+
+# Author : Susan Paykin
+# Date : March 15, 2021
+# About: This code prepares the metrics on Access to Pharmacies for zip codes and census tracts. 
+
+# Part 1) Wrangle national MOUD location data
+
+# Part 2) Conduct nearest resource analysis, using minimum distance as proxy for access, 
+# determining the distance from a) census tracts and b) zip codes to MOUDs
+
+# Part 3) Save final Access Metrics: 
+# Create final csv files with access metrics by census tracts and zip codes, saved in the data_final folder. 
+
+#### Part 1) Wrangle national MOUD location data ----
+
+# Set up 
+library(tidyverse)
+library(sf)
+library(tmap)
+library(units)
+
+setwd("~/git/opioid-policy-scan")
+
+# Load full MOUD dataset
+mouds <- st_read("Policy_Scan/data_raw/us-wide-moudsCleaned.gpkg") %>%
+  st_transform(3857)
+
+# Subset MOUD by category
+bup <- mouds %>% filter(category == "buprenorphine")
+meth <- mouds %>% filter(category == "methadone")
+nalViv <- mouds %>% filter(category == "naltrexone/vivitrol")
+
+# Read in location data
+zips <- read_sf("Policy_Scan/data_final/geometryFiles/tl_2018_zcta/zctas2018.shp") %>%
+  st_transform(3857)
+tracts <- read_sf("Policy_Scan/data_final/geometryFiles/tl_2018_tract/tracts2018.shp") %>%
+  st_transform(3857)
+
+#### Part 2) Nearest Resource Analysis, Minimum Distance ----
+
+#### Calculate Centroids - Zip Codes ----
+
+# Create centroids for zip codes
+zipCentroids <- st_centroid(zips)
+
+#### All MOUD ----
+
+# Identify MOUD that is closest to zip centroid, then subset by index to get nearest location
+nearestMOUD_index <- st_nearest_feature(zipCentroids, mouds)
+nearestMOUD <- mouds[nearestMOUD_index, ]
+
+# Calculate distance
+minDistZips <- st_distance(zipCentroids, nearestMOUD, by_element = TRUE)
+head(minDistZips) # meters
+
+# Change from meters to miles
+minDistZips_mi <- set_units(minDistZips, "mi")
+head(minDistZips_mi)
+
+#### Buprenorphine ----
+
+nearestbup_index <- st_nearest_feature(zipCentroids, bup)
+nearestbup <- bup[nearestbup_index, ]
+minDistBup <- st_distance(zipCentroids, nearestbup, by_element = TRUE)
+minDistBup <- set_units(minDistBup, "mi")
+head(minDistBup)
+
+#### Methadone ----
+
+nearestMeth_index <- st_nearest_feature(zipCentroids, meth)
+nearestMeth <- meth[nearestMeth_index, ]
+minDistMeth <- st_distance(zipCentroids, nearestMeth, by_element = TRUE)
+minDistMeth <- set_units(minDistMeth, "mi")
+head(minDistMeth)
+
+#### NalViv -----
+
+nearestnalViv_index <- st_nearest_feature(zipCentroids, nalViv)
+nearestnalViv <- nalViv[nearestnalViv_index, ]
+minDistnalViv <- st_distance(zipCentroids, nearestnalViv, by_element = TRUE)
+minDistnalViv <- set_units(minDistnalViv, "mi")
+head(minDistnalViv)
+
+# Merge data
+minDistZips_clean <- cbind(zips, minDistZips_mi, minDistBup, minDistMeth, minDistnalViv)
+head(minDistZips_clean)
+
+# Clean up data
+minDistZips_clean <- minDistZips_clean %>% select(GEOID = GEOID10, minDistZ = minDistZips_mi, minDistBup, minDistMeth, minDistNalViv = minDistnalViv)
+head(minDistZips_clean)
+
+#### Calculate Centroids - Census Tracts ----
+
+# Create centroids for tracts
+tractCentroids <- st_centroid(tracts)
+
+# All MOUD ----
+
+# Identify MOUD that is closest to tract centroid, then subset by index to get nearest location
+nearestMOUD_tract_index <- st_nearest_feature(tractCentroids, mouds)
+nearestMOUD_tract <- mouds[nearestMOUD_tract_index, ]
+
+# Calculate distance
+minDistTracts <- st_distance(tractCentroids, nearestMOUD_tract, by_element = TRUE)
+head(minDistTracts) # meters
+
+# Change from meters to miles
+minDistTracts_mi <- set_units(minDistTracts, "mi")
+head(minDistTracts_mi)
+
+#### Buprenorphine ----
+
+nearestbup_index <- st_nearest_feature(tractCentroids, bup)
+nearestbup <- bup[nearestbup_index, ]
+minDistBup <- st_distance(tractCentroids, nearestbup, by_element = TRUE)
+minDistBup <- set_units(minDistBup, "mi")
+head(minDistBup)
+
+#### Methadone ----
+
+nearestMeth_index <- st_nearest_feature(tractCentroids, meth)
+nearestMeth <- meth[nearestMeth_index, ]
+minDistMeth <- st_distance(tractCentroids, nearestMeth, by_element = TRUE)
+minDistMeth <- set_units(minDistMeth, "mi")
+head(minDistMeth)
+
+#### NalViv -----
+
+nearestnalViv_index <- st_nearest_feature(tractCentroids, nalViv)
+nearestnalViv <- nalViv[nearestnalViv_index, ]
+minDistnalViv <- st_distance(tractCentroids, nearestnalViv, by_element = TRUE)
+minDistnalViv <- set_units(minDistnalViv, "mi")
+head(minDistnalViv)
+
+# Merge data
+minDistTracts_clean <- cbind(tracts, minDistTracts_mi, minDistBup, minDistMeth, minDistnalViv)
+head(minDistTracts_clean)
+
+# Clean up data
+minDistTracts_clean <- minDistTracts_clean %>% select(GEOID, STATEFP, COUNTYFP, TRACTCE, 
+                                                      minDistT = minDistTracts_mi, minDistBup, minDistMeth, 
+                                                      minDistNalViv = minDistnalViv)
+head(minDistTracts_clean)
+
+#### Part 3) Save final Access Metrics ----
+
+# Save zip code access metrics
+write_sf(minDistZips_clean, "Policy_Scan/data_final/Access01_Z.csv")
+
+# Save tract access metrics
+write_sf(minDistTracts_clean, "Policy_Scan/data_final/Access01_T.csv")
