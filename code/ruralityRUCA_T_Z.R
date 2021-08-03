8959185e457c2ac46fdf25a778bc69f80ffc9054
spaykin <spaykin@uchicago.edu>
2021-08-03 11:17:47 -0500

    rurality code revision

--- code/ruralityRUCA_T_Z.R
+++ code/ruralityRUCA_T_Z.R
@@ -1,24 +1,42 @@
 # Author : Moksha Menghaney
 # Date : October 27th, 2020
-# This piece of code will generate tract & zipcode level urban/suburban/rural classification 
-# for policy scan, files HS02
-# It also generates county level % rurality metrics for policy scan which is stored 
-# in raw files folder for further processing.
+# This piece of code will generate tract & zipcode level urban/suburban/rural classification for OEPS, files HS02
+# It also generates county level % rurality metrics which is stored in raw files folder for further processing.
 
-
+### Set up ----
 library(xlsx)
 library(tidyverse)
 
-geometryFilesLoc <- './opioid-policy-scan/Policy_Scan/data_final/geometryFiles/'
-rawDataFilesLoc <- './opioid-policy-scan/Policy_Scan/data_raw/'
-outputFilesLoc <- './opioid-policy-scan/Policy_Scan/data_final/'
+geometryFilesLoc <- './opioid-policy-scan/data_final/geometryFiles/'
+rawDataFilesLoc <- './opioid-policy-scan/data_raw/'
+outputFilesLoc <- './opioid-policy-scan/data_final/'
 
-#classifications finalized
+# Classifications finalized
 urban <- c(1.0, 1.1)
 suburban <- c(2.0, 2.1, 4.0, 4.1)
 # everything else rural
 
-### RUCA AT ZCTA LEVEL
+### TRACT LEVEL ----
+
+rucaTract <- openxlsx::read.xlsx(paste0(rawDataFilesLoc,'ruca2010revisedTract.xlsx'), 
+                                 sheet = 1, startRow = 2, colNames = TRUE)
+
+colnames(rucaTract) <- c('countyFIPS','State','County','tractFIPS','RUCA1',
+                         'RUCA2','Pop_2010','Area_2010','PopDen_2010')
+
+rucaTract$rurality <- ifelse(rucaTract$RUCA2 %in% urban, "Urban",
+                             ifelse(rucaTract$RUCA2 %in% suburban, "Suburban", "Rural"))
+
+rucaTract$rurality <- factor(rucaTract$rurality , levels= c('Urban','Suburban','Rural'))
+
+write.csv(rucaTract %>% 
+            select(tractFIPS, RUCA1, RUCA2, rurality) %>%
+            mutate(RUCA1 = as.character(RUCA1),
+                   RUCA2 = as.character(RUCA2)),
+          paste0(outputFilesLoc,'HS02_RUCA_T.csv'), row.names = FALSE)
+
+#### ZCTA LEVEL ----
+
 rucaZipcode <- read.xlsx(paste0(rawDataFilesLoc,'RUCA2010zipcode.xlsx'), 
                          sheetName = 'Data', header = TRUE) %>% select(-c(STATE,ZIP_TYPE))
 
@@ -31,23 +49,8 @@ rucaZipcode <- rucaZipcode %>%
 write.csv(rucaZipcode,paste0(outputFilesLoc,'HS02_RUCA_Z.csv'),
                              row.names = FALSE)
 
-### RUCA AT TRACT LEVEL
-rucaTract <- openxlsx::read.xlsx(paste0(rawDataFilesLoc,'ruca2010revisedTract.xlsx'), 
-                                 sheet = 1, startRow = 2, colNames = TRUE)
-colnames(rucaTract) <- c('countyFIPS','State','County','tractFIPS','RUCA1',
-                         'RUCA2','Pop_2010','Area_2010','PopDen_2010')
+### COUNTY LEVEL ----
 
-rucaTract$rurality <- ifelse(rucaTract$RUCA2 %in% urban, "Urban",
-                             ifelse(rucaTract$RUCA2 %in% suburban, "Suburban", "Rural"))
-rucaTract$rurality <- factor(rucaTract$rurality , levels= c('Urban','Suburban','Rural'))
-
-write.csv(rucaTract %>% 
-            select(tractFIPS, RUCA1, RUCA2, rurality) %>%
-            mutate(RUCA1 = as.character(RUCA1),
-                   RUCA2 = as.character(RUCA2)),
-          paste0(outputFilesLoc,'HS02_RUCA_T.csv'), row.names = FALSE)
-
-### RUCA AT COUNTY LEVEL
 # calculate % of tracts in county rural, urban, suburban
 rucaCountyRurality <- rucaTract %>% 
                       select(countyFIPS, rurality) %>% 
@@ -61,7 +64,7 @@ rucaCountyRurality <- pivot_wider(rucaCountyRurality,id_cols = 'countyFIPS',
                       mutate(check = round(sum(Urban+Suburban+Rural),2))
 
 
-## check data and clean up
+## Check data and clean up
 rucaCountyRurality[which(rucaCountyRurality$check !=1),]
 rucaCountyRurality <- data.frame(rucaCountyRurality %>% 
                                    mutate(Urban = round(Urban,2),
